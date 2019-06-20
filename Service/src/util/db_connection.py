98e0aebd3b7eb9788f8adb4ca1c4b63ecff387d8
asnewman <ashznewman@gmail.com>
2019-06-19 22:54:37 -0700

    moved and reformatted config file

--- Service/src/util/db_connection.py
+++ Service/src/util/db_connection.py
@@ -2,26 +2,19 @@ import pymysql
 import os
 import simplejson as json
 
-CONFIG_FILE_PATH = 'Service/db/config.json'
-ROOT_FOLDER_NAME = 'stockdog-api/'
-
-def getConfigFilePath():
-   cwd = os.getcwd()
-   strIdx = cwd.find(ROOT_FOLDER_NAME)
-   return cwd[:strIdx + len(ROOT_FOLDER_NAME)] + CONFIG_FILE_PATH
-
+CONFIG_FILE_PATH = './config.json'
 
 def getDBConn(envType='local'):
    if envType == 'local':
       try:
-         configFile = open(getConfigFilePath(), 'r')
+         configFile = open(CONFIG_FILE_PATH, 'r')
          config = json.load(configFile)
          configFile.close()
       except Exception as e:
          raise Exception('The db config filename was not provided or poorly formatted') 
 
-      conn = pymysql.connect(host='127.0.0.1', user=config['user'], password=config['password'], 
-         database=config['database'], cursorclass=pymysql.cursors.DictCursor, autocommit=True)
+      conn = pymysql.connect(host='127.0.0.1', user=config['db']['user'], password=config['db']['password'], 
+         database=config['db']['database'], cursorclass=pymysql.cursors.DictCursor, autocommit=True)
    else:
       conn = pymysql.connect(host='127.0.0.1', user='root', password='', 
          database='StockDog', cursorclass=pymysql.cursors.DictCursor, autocommit=True)
