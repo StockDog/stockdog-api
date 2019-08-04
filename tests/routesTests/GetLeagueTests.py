19082f3760f103a4b6e225fd5a83e1715cff6b6d
asnewman <ashznewman@gmail.com>
2019-08-04 10:53:08 -0700

    Added error tests

--- tests/routesTests/GetLeagueTests.py
+++ tests/routesTests/GetLeagueTests.py
@@ -80,5 +80,48 @@ class GetLeagueTests(TestConfiguration):
         self.assertEquals(responseData.portfolios[0].name, "david")
         self.assertEquals(type(responseData.portfolios[0].value), int)
 
+    def test_get_league_non_existant_id(self):
+        response = requests.get(url=f"{self.leagueBaseUrl}/2",
+            headers=self.headers)
+        responseData = self.getJson(response)
+
+        self.assertEquals(response.status_code, 404)
+
+    def test_get_league_unauthorized_get(self):
+        response = requests.get(url=f"{self.leagueBaseUrl}/1",
+            headers=self.headers)
+        responseData = self.getJson(response)
+
+        registerUrl = self.baseUrl + '/users'
+        registerBody = {
+            'firstName' : 'NotDave',
+            'lastName' : 'NotJanzen',
+            'email' : 'daveeee.janzen18@gmail.com',
+            'password' : 'Stockd2g'
+        }
+        registerResponse = requests.post(url=registerUrl, data=json.dumps(registerBody), headers=self.headers)
+        registerResponseData = self.getJson(registerResponse)
+        self.assertEqual(registerResponse.status_code, 200)
+        self.assertTrue('id' in registerResponseData)
+        self.assertTrue(registerResponseData['id'] > 0)
+      
+        loginUrl = self.baseUrl + '/users/session'
+        loginBody = {
+            'email' : 'daveeee.janzen18@gmail.com',
+            'password' : 'Stockd2g'
+        }
+        loginResponse = requests.post(url=loginUrl, data=json.dumps(loginBody), headers=self.headers)
+        loginResponseData = self.getJson(loginResponse)
+        self.assertEqual(loginResponse.status_code, 200)
+        self.assertIsNotNone(loginResponseData['userId'])
+        self.assertIsNotNone(loginResponseData['token'])
+
+        token = loginResponseData['token']
+        self.headers['Authorization'] = 'token ' + token
+
+        response = requests.get(url=f"{self.leagueBaseUrl}/1",
+            headers=self.headers)
+        self.assertEqual(response.status_code, 403)
+
     def tearDown(self):
       self.deleteTables(['Transaction', 'PortfolioItem', 'Portfolio', 'User'])
