489e68dd98ac639d121390a82ce1eed0960aafb7
asnewman <ashznewman@gmail.com>
2019-11-04 23:44:39 -0800

    Fixed db env vars

--- util/db_connection.py
+++ util/db_connection.py
@@ -2,7 +2,11 @@ import pymysql
 import os
 
 def getDBConn():
-   conn = pymysql.connect(host='127.0.0.1', user="sduser", password="sdpass",
-      database="StockDog", cursorclass=pymysql.cursors.DictCursor, autocommit=True)
-   
+   host = os.getenv("db.host") or "127.0.0.1"
+   user = os.getenv("db.user") or "sduser"
+   password = os.getenv("db.password") or "sdpass"
+   database = os.getenv("db.database") or "StockDog"
+
+   conn = pymysql.connect(host=host, user=user, password=password,
+                          database=database, cursorclass=pymysql.cursors.DictCursor, autocommit=True)
    return conn
